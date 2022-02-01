48d852525ff0df575725e00ac32fc6e862dcf063
ImLumi <mikesimon150@gmail.com>
2022-02-01 23:57:41 +0100

    add Todo class and test main.js #15

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,10 @@
 import todoList from './fakeTodoDb.js';
+import Todo from './Todo.js';
+
+const todoContainer = document.querySelector('.todo-container');
+todoContainer.innerHTML = '';
+
+todoList.forEach(({ todoText, time, isCompleted }) => {
+  const todoElement = new Todo(todoText, time, isCompleted);
+  todoElement.addTodoToHTMLElement(todoContainer);
+});
