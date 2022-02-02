cd339c9ace7b5c6d1b0e3a00c4d2bdff711d5074
ImLumi <mikesimon150@gmail.com>
2022-02-02 23:40:07 +0100

    add more todo

--- public/js/fakeTodoDb.js
+++ public/js/fakeTodoDb.js
@@ -14,6 +14,21 @@ const todoList = [
     time: '2022-02-11T22:38',
     isCompleted: true,
   },
+  {
+    todoText: 'Megnyerni a versenyt.',
+    time: '2022-02-08T22:00',
+    isCompleted: false,
+  },
+  {
+    todoText: 'Elkezdeni a Todo app fejlesztését',
+    time: '2022-02-03T22:38',
+    isCompleted: false,
+  },
+  {
+    todoText: 'Alma',
+    time: '2022-02-11T22:38',
+    isCompleted: true,
+  },
 ];
 
 export default todoList;
